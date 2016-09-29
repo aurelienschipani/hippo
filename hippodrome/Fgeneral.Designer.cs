521ae220ec0f9ffdebad3add84cf7b6cbbfc85cf
AnthonyMasse <anthony.masse13@gmail.com>
2016-09-29 14:20:16 +0200

    V0.1

--- hippodrome/Fgeneral.Designer.cs
+++ hippodrome/Fgeneral.Designer.cs
@@ -44,10 +44,12 @@
             this.dgCourses = new System.Windows.Forms.DataGridView();
             this.tabgeneral = new System.Windows.Forms.TabControl();
             this.tabCheval = new System.Windows.Forms.TabPage();
+            this.btAntoHippo = new System.Windows.Forms.Button();
             this.tabCourse.SuspendLayout();
             this.gbDetailCourse.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgCourses)).BeginInit();
             this.tabgeneral.SuspendLayout();
+            this.tabCheval.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabCourse
@@ -203,6 +205,7 @@
             // 
             // tabCheval
             // 
+            this.tabCheval.Controls.Add(this.btAntoHippo);
             this.tabCheval.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabCheval.Location = new System.Drawing.Point(4, 28);
             this.tabCheval.Name = "tabCheval";
@@ -211,6 +214,15 @@
             this.tabCheval.TabIndex = 0;
             this.tabCheval.Text = "Les chevaux";
             this.tabCheval.UseVisualStyleBackColor = true;
+            // 
+            // btAntoHippo
+            // 
+            this.btAntoHippo.Location = new System.Drawing.Point(33, 52);
+            this.btAntoHippo.Name = "btAntoHippo";
+            this.btAntoHippo.Size = new System.Drawing.Size(155, 80);
+            this.btAntoHippo.TabIndex = 0;
+            this.btAntoHippo.Text = "Dorian";
+            this.btAntoHippo.UseVisualStyleBackColor = true;
             // 
             // Fgeneral
             // 
@@ -226,6 +238,7 @@
             this.gbDetailCourse.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgCourses)).EndInit();
             this.tabgeneral.ResumeLayout(false);
+            this.tabCheval.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -248,6 +261,7 @@
         private System.Windows.Forms.DataGridView dgCourses;
         private System.Windows.Forms.TabControl tabgeneral;
         private System.Windows.Forms.TabPage tabCheval;
+        private System.Windows.Forms.Button btAntoHippo;
 
     }
 }
